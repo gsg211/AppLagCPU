dedf8409130c3e2b7fedb881524d5d5ecb3f4823
gsg211 <goreagabi8@gmail.com>
2026-05-09 11:59:10 +0300

    timing

---
+++ src/Timing/Timing.cpp
@@ -0,0 +1,31 @@
+//
+// Created by sabin on 5/9/26.
+//
+//
+// Created by tudor on 5/9/26.
+//
+#include "Timing.h"
+#include "../../Constants.h"
+
+constexpr const uint16_t CLOCK_DELAY_MSECS = 5;
+
+Timing::Timing()
+{}
+
+void Timing::start() {
+    m_timingThread = std::jthread(&Timing::run, this);
+}
+
+void Timing::stop() {
+    bool newIsRunning = false;
+    m_running.compare_exchange_strong(newIsRunning, m_running);
+}
+
+void Timing::run() {
+    while (m_running.load()) {
+        m_tick.store(false);
+        std::this_thread::sleep_for(std::chrono::milliseconds(CLK_PERIOD_ms));
+        m_tick.store(true);
+        m_timingConditionVariable.notify_all();
+    }
+}
---
+++ src/Timing/Timing.h
@@ -0,0 +1,39 @@
+//
+// Created by tudor on 5/9/26.
+//
+
+#ifndef AMD_APPLAG_TIMING_H
+#define AMD_APPLAG_TIMING_H
+
+#include <condition_variable>
+#include <thread>
+
+class Timing {
+public:
+    Timing();
+    Timing(const Timing&) = delete;
+    Timing(const Timing&&) = delete;
+    Timing& operator=(const Timing&) = delete;
+    Timing& operator=(const Timing&&) = delete;
+    ~Timing() = default;
+
+public:
+    void start();
+    void stop();
+    std::condition_variable& TimingConditionVariable() { return m_timingConditionVariable; }
+    std::mutex& TimingMutex() { return m_timingMutex; }
+    bool IsActive() { return m_tick.load(); }
+
+private:
+    void run();
+
+private:
+    std::mutex m_timingMutex;
+    std::condition_variable m_timingConditionVariable;
+    std::atomic_bool m_tick = false;
+
+    std::jthread m_timingThread;
+    std::atomic_bool m_running = true;
+};
+
+#endif //AMD_APPLAG_TIMING_H
